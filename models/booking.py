650b93e731d3a359475903566a1d16db0be0e223
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-04-22 19:03:31 +0100

    classes done

---
+++ models/booking.py
@@ -0,0 +1,7 @@
+class Booking:
+    def __init__(self, name, num_of_group, address, photo_type, id=None):
+        self.name = name
+        self.num_of_group = num_of_group
+        self.address = address
+        self.photo_type = photo_type
+        self.id = id
---
+++ models/service.py
@@ -0,0 +1,7 @@
+class Service:
+    def __init__(self, photo_type, hours, place, price, id=None):
+        self.photo_type = photo_type
+        self.hours = hours
+        self.place = place
+        self.price = price
+        self.id = id
