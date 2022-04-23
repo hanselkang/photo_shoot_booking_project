7dc5047b1ed50f6c8ba16d28d1e725ba53549333
Hansel Kang <43307207+hanselkang@users.noreply.github.com>
2022-04-23 12:29:54 +0100

    added new for all tables

--- models/service.py
+++
@@ -1,8 +0,0 @@
-class Service:
-    def __init__(self, photo_type, place, hours, price, photographer, id=None):
-        self.photo_type = photo_type
-        self.place = place
-        self.hours = hours
-        self.price = price
-        self.photographer = photographer
-        self.id = id
